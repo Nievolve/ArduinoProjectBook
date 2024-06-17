2c9eceb695340bb5e95a01e1e485d605c658d0a2
Andreas <131877497+Nievolve@users.noreply.github.com>
2024-06-17 11:44:34 +0200

    Update Countdown.ino

--- Eget/Countdown/Countdown.ino
+++ Eget/Countdown/Countdown.ino
@@ -1,6 +1,6 @@
 
 void setup() {
-  for(int pinNumber = 2; pinNumber<12; pinNumber++){
+  for(int pinNumber = 0; pinNumber=9; pinNumber++){
     pinMode(pinNumber, OUTPUT);
     digitalWrite(pinNumber, LOW);
 
@@ -9,12 +9,14 @@ void setup() {
 
 
 void loop() {
-
+//Turn all LED off
+for (int ledNumber = 0; ledNumber=9; ledNumber++){ 
+    digitalWrite(ledNumber, LOW);
 int buttonPressed = digitalRead(13);
   if (buttonPressed == HIGH){
 
-  
-  for (int ledNumber = 1; ledNumber<=12; ledNumber++){
+  //Turn every LED on with a delay of 1.000 second
+  for (int ledNumber = 0; ledNumber=9; ledNumber++){
     digitalWrite(ledNumber, HIGH);
     delay(1000)
   } 
