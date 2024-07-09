71436ec73925480cd592ed30aec5792fccdcc1c3
Nievolve <1990andreaslindblad@gmail.com>
2024-07-09 14:10:17 +0200

    added new project

---
+++ TouchLight/TouchLight.ino
@@ -0,0 +1,27 @@
+#include <CapacitiveSensor.h>
+CapacitiveSensor capSensor = CapacitiveSensor(4,2);
+
+int treshold = 1000;
+const int ledPin = 12;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  
+
+}
+
+void loop() {
+  long sensorValue = capSensor.capacitiveSensor(50);
+  Serial.println(sensorValue);
+
+  if(sensorValue > treshold){
+    digitalWrite(ledPin, HIGH);
+  
+  }
+  else{
+    digitalWrite(ledPin, LOW);
+
+  }
+  delay(10);
+}
